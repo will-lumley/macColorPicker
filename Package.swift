0dd247892f8a85d878c6b6e514b4f9b014dd3fc4
William Lumley <wlumley95@gmail.com>
2021-05-03 02:11:19 +1000

    Added SPM Package.swift.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ColorPicker",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ColorPicker",
+            targets: ["ColorPicker"]),
+    ]
+)
