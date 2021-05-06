afef3df723685d7e0c5470d66406c899d9f86e14
William Lumley <wlumley95@gmail.com>
2021-05-07 00:21:30 +1000

    Renamed to macColorPicker.

--- Package.swift
+++ Package.swift
@@ -18,3 +18,27 @@ let package = Package(
         
     ]
 )
+
+let package = Package(
+    name: "ColorPicker",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ColorPicker",
+            targets: ["ColorPicker"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "ColorPicker",
+            dependencies: []),
+        .testTarget(
+            name: "ColorPickerTests",
+            dependencies: ["ColorPicker"]),
+    ]
+)
