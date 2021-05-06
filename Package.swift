77c1ab3b820c92cfa07983b326c6e208b2f9e855
William Lumley <wlumley95@gmail.com>
2021-05-07 00:22:47 +1000

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,31 +1,13 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
-
 let package = Package(
-    name: "ColorPicker",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "ColorPicker",
-            targets: ["ColorPicker"]),
-    ],
-    dependencies: [
-
-    ],
-    targets: [
-        
-    ]
-)
-
-let package = Package(
-    name: "ColorPicker",
+    name: "macColorPicker",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ColorPicker",
-            targets: ["ColorPicker"]),
+            name: "macColorPicker",
+            targets: ["macColorPicker"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -35,10 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ColorPicker",
-            dependencies: []),
-        .testTarget(
-            name: "ColorPickerTests",
-            dependencies: ["ColorPicker"]),
+            name: "macColorPicker",
+            dependencies: [])
     ]
 )
