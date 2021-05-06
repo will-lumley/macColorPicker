218862721e7ce910c61d9c2e0023dc5bc78fc42b
William Lumley <wlumley95@gmail.com>
2021-05-07 00:39:55 +1000

    Updated sources parent directory name.

--- Example/Pods/Target Support Files/Pods-macColorPicker_Example/Pods-macColorPicker_Example-umbrella.h
+++ Example/Pods/Target Support Files/Pods-macColorPicker_Example/Pods-macColorPicker_Example-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ColorPicker_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ColorPicker_TestsVersionString[];
+FOUNDATION_EXPORT double Pods_macColorPicker_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_macColorPicker_ExampleVersionString[];
 
